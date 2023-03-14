ae72d5b78889b0c91fab1c3f9126f0a156a0fc1f
mogge1337 <morgan.burlin@gmail.com>
2023-03-14 09:25:54 +0100

    appp cssss

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Footer from "./components/Footer";
 
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
+import './App.css';
+
 function App() {
   return <div class="grid-container">
     <Header />
